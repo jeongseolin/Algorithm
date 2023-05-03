389afba48b1cd81c31c9625f841f82bd01bdf87c
mungseo1012 <125359051+jeongseolin@users.noreply.github.com>
2023-05-03 23:40:38 +0900

    [unrated] Title: 1로 만들기, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.js
@@ -0,0 +1,16 @@
+ function solution(num_list) {
+    let sum = 0;
+    for(let i = 0; i < num_list.length; i++) {
+        while(1) {
+            if(num_list[i] === 1) break;
+            if(num_list[i] % 2 === 0) {
+                num_list[i] = num_list[i] / 2;
+                sum += 1;
+            } else if(num_list[i] % 2 === 1) {
+                num_list[i] = (num_list[i] - 1) / 2;
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
