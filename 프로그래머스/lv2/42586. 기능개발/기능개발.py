736fd708e2e6f7d91f31e400497d534f27cecc41
mungseo1012 <125359051+jeongseolin@users.noreply.github.com>
2023-05-17 00:19:15 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.py
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.py
@@ -4,14 +4,14 @@ def solution(progresses, speeds):
     develop = 0
     
     while len(progresses) > 0:
-        if progresses[0] + play * speeds[0] >= 100:
+        if progresses[0] + play * speeds[0] >= 100: #개발완료
             progresses.pop(0)
             speeds.pop(0)
             develop += 1
-        else :
-            if develop > 0:
+        else : #개발중
+            play += 1
+            if develop > 0: #이전에 기능이 개발되었다면
                 answer.append(develop)
                 develop = 0
-            play += 1
     answer.append(develop)
     return answer
